c2d5badcd45feb2dd75f0f9129fd9ac38e010826
mhmtkarakas <m.karakas32@gmail.com>
2024-07-11 21:15:14 +0300

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <AssignmentIcon />
+        TODO APP</h1>
+    </header>
+  )
+}
+
+export default Header
